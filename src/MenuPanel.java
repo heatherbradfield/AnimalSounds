3e16a2b96e57e3989a06fda6afc2aaa7fd77962b
FALLENxGaLaXie5 <steward.joshua@rocketmail.com>
2016-09-21 02:20:30 -0700

    changes to instructions panel and main menu

--- src/MenuPanel.java
+++ src/MenuPanel.java
@@ -1,8 +1,10 @@
 import javax.swing.*;
+import java.applet.Applet;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.ComponentAdapter;
+import java.net.URL;
 import javax.swing.Icon;
 import javax.swing.ImageIcon;
 
@@ -82,8 +84,8 @@ public class MenuPanel extends JPanel {
 class InstructPanel extends JPanel{
     public InstructPanel(final Animal newanimal){
 
-
-        this.setLayout(new SpringLayout());
+        //stateImgBut[0].setVisible(false);
+        this.setLayout(new GridBagLayout());
         GridBagConstraints gbc = new GridBagConstraints();
         gbc.gridwidth = GridBagConstraints.REMAINDER;
         gbc.fill = GridBagConstraints.HORIZONTAL;
@@ -93,18 +95,72 @@ class InstructPanel extends JPanel{
         instrLabel.setForeground(Color.RED);
         this.add(instrLabel, gbc);
 
-        add(new JTextArea("Animal Sounds Will Play As Follows:"), gbc);
+        add(new JTextArea("Match the Sound to Each Animal, and if correct, that animal will disappear. Click below to see progression:"), gbc);
         Icon arrIcon = new ImageIcon(getClass().getResource("Images/arrow.png"));
         ImageIcon arrow = new ImageIcon("Images/arrow.png");
         JLabel arrLabel = new JLabel("", arrIcon, JLabel.CENTER);
+        arrLabel.setMaximumSize(new Dimension(200, 100));
+        arrLabel.setPreferredSize(new Dimension(190, 80));
+        arrLabel.setMinimumSize(new Dimension(150, 70));
         add(arrLabel, gbc);
 
+
+
+        ImageIcon[] stateImg = new ImageIcon[]{   //put .png in array
+                new ImageIcon(getClass().getResource("Images/firstState.png")),
+                new ImageIcon(getClass().getResource("Images/secState.png")),
+                new ImageIcon(getClass().getResource("Images/finalState.png")),
+        };
+
+
+
+            JButton[] stateImgBut = new JButton[]
+                {
+                        new JButton(stateImg[0]),
+                        new JButton(stateImg[1]),
+                        new JButton(stateImg[2]),
+                };
+
+
+        for (int i = 0; i < stateImg.length; i++)
+        {
+            stateImgBut[i].setSize(new Dimension(50,20));
+            stateImgBut[i].setVisible(false);
+            stateImgBut[i].setOpaque(false);
+            stateImgBut[i].setBorderPainted(false);
+            stateImgBut[i].setContentAreaFilled(false);
+            stateImgBut[i].setFocusPainted(false);
+        }
+        stateImgBut[0].setVisible(true);
+        stateImgBut[0].addActionListener(new ActionListener() {
+
+            public void actionPerformed(ActionEvent e)
+            {
+                stateImgBut[0].setVisible(false);
+                stateImgBut[1].setVisible(true);
+                stateImgBut[1].addActionListener(new ActionListener() {
+                    public void actionPerformed(ActionEvent e) {
+                        stateImgBut[1].setVisible(false);
+                        stateImgBut[2].setVisible(true);
+                        stateImgBut[2].addActionListener(new ActionListener()
+                        {
+                            public void actionPerformed(ActionEvent e)
+                            {
+                                newanimal.swapCard(Animal.menuPan);
+                            }
+                        });
+                    }
+                });
+            }
+        });
+
+        add(stateImgBut[0], gbc);
+        add(stateImgBut[1], gbc);
+        add(stateImgBut[2], gbc);
         JButton menu= new JButton("Back to Menu");
-        menu.addActionListener(new ActionListener(){
-            /**
-             * Returns back to Main Menu
-             * @param e the mouseClicked or mouseClicked event
-             */
+        menu.addActionListener(new ActionListener()
+        {
+
             public void actionPerformed(ActionEvent e){
                 newanimal.swapCard(Animal.menuPan);
             }
